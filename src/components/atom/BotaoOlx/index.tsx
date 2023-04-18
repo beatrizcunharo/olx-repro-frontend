e241e5e1677465452ccbf6ca8dcdaba910733649
beatrizcunharo <beatrizcunharo@gmail.com>
2023-04-18 01:55:50 -0300

    feat: Adicionando função onClick ao botão e renomeando para botão Olx

--- src/components/atom/BotaoOlx/index.tsx
+++ src/components/atom/BotaoOlx/index.tsx
@@ -1,21 +1,24 @@
 import './styles.scss'
 
-interface BotaoCmsProps {
+interface BotaoOlxProps {
     cor?:  "primaria" | "secundaria"
     tamanho?: "medio" | "grande"
     texto: string
     style?: string
+    onClick: (props: any) => any
 }
 
-export const BotaoCms = (props: BotaoCmsProps) => {
-    const { cor, texto, style, tamanho } = props
+export const BotaoOlx = (props: BotaoOlxProps) => {
+    const { cor, texto, style, tamanho, onClick} = props
 
     return (
         <button
-            className={`botaoCms 
+            className={`botaoOlx 
                 ${style} ${cor && cor === "primaria" ? "cor_primaria" : cor === "secundaria" ? "cor_secundaria" : ""}
                 ${tamanho && tamanho === "medio" ? "tam_medio" : tamanho === "grande" ? "tam_grande" : ""}
             `}
+            aria-label={texto}
+            onClick={onClick}
         >
             {texto}
         </button>
